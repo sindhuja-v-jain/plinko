ad273421db7b00151170f0db46eea2e24547a014
sindhuja-v-jain <69973165+sindhuja-v-jain@users.noreply.github.com>
2020-10-24 04:07:26 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,52 +6,27 @@ var plinkos=[];
 var particles=[];
 var divisions =[];
 var divisionHeight;
+
 function setup() {
   createCanvas(windowWidth,windowHeight);
   engine = Engine.create()
   world = engine.world;
   ground = new Ground(width/2 , height-10 , width , 10)
-  divisionHeight=height/3;
+  divisionHeight=height/3+50;
   for ( var k= 0; k<=width; k=k+width/6){
     divisions.push(new Ground(k, height-divisionHeight/2-15,10, divisionHeight))
   }
-  for(var j =40; j<=width;  j=j+50 )
-  {
-    plinkos.push(new Plinko(j, 75))
-  }
-  for(var  j=15; j<= width-10;j=j+50)
-  {
-   plinkos.push( new Plinko(j , 125))
-  }
-  for(var j =40; j<=width;  j=j+50 )
-  {
-    plinkos.push(new Plinko(j, 175))
-  }
-  for(var  j=15; j<= width-10;j=j+50)
-  {
-   plinkos.push( new Plinko(j , 225))
-  }
-  for(var j =40; j<=width;  j=j+50 )
-  {
-    plinkos.push(new Plinko(j, 275))
-  }
-  for(var  j=15; j<= width-10;j=j+50)
-  {
-   plinkos.push( new Plinko(j , 325))
-  }
-  for(var j =40; j<=width;  j=j+50 )
-  {
-    plinkos.push(new Plinko(j, 375))
-  }
-  for(var  j=15; j<= width-10;j=j+50)
-  {
-   plinkos.push( new Plinko(j , 425))
-  }
-  for(var j =40; j<=width;  j=j+50 )
-  {
-    plinkos.push(new Plinko(j, 475))
-  }
+  for( var l = 75 ; l<=height-height/3-100 ; l=l+100){
+    for(var j =40; j<=width;  j=j+50 )
+    {
+      plinkos.push(new Plinko(j, l))
+    }
+    for(var  k=15; k<= width-10;k=k+50)
+    {
+     plinkos.push( new Plinko(k , l+50))
+    }
 
+  }
 }
 
 function draw() {
